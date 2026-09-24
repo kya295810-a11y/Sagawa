a0ce73b2a7f5813b417af050061f720e2a4f5638
Kyaw san lin <kya295810@gmail.com>
2026-09-24 18:00:11 +0800

    Add regional exchange data model

---
+++ malay-mm-admin/server/migrations/015_regional_exchange.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+ALTER TABLE exchange_rates
+  ADD COLUMN IF NOT EXISTS country_code varchar(2) NOT NULL DEFAULT 'MY',
+  ADD COLUMN IF NOT EXISTS base_currency varchar(3) NOT NULL DEFAULT 'MYR';
+
+UPDATE exchange_rates
+SET country_code = 'MY',
+    base_currency = 'MYR'
+WHERE country_code IS NULL
+   OR base_currency IS NULL;
+
+ALTER TABLE exchange_provider_rates
+  ADD COLUMN IF NOT EXISTS country_code varchar(2) NOT NULL DEFAULT 'MY',
+  ADD COLUMN IF NOT EXISTS base_currency varchar(3) NOT NULL DEFAULT 'MYR';
+
+UPDATE exchange_provider_rates
+SET country_code = 'MY',
+    base_currency = 'MYR'
+WHERE country_code IS NULL
+   OR base_currency IS NULL;
+
+CREATE INDEX IF NOT EXISTS exchange_rates_country_latest_idx
+  ON exchange_rates (country_code, updated_at DESC, id DESC);
+
+CREATE INDEX IF NOT EXISTS exchange_provider_rates_country_display_idx
+  ON exchange_provider_rates (country_code, published, display_order, id);
+
+COMMIT;
