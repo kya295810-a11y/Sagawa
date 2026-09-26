5cd1e3bcf44768482d9b3f75676bdb4ecc345718
Kyaw san lin <kya295810@gmail.com>
2026-09-26 09:51:04 +0800

    Add signup location fields

---
+++ malay-mm-admin/server/migrations/014_signup_location_and_verified_password.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+ALTER TABLE profiles
+  ADD COLUMN IF NOT EXISTS date_of_birth date,
+  ADD COLUMN IF NOT EXISTS country_code char(2),
+  ADD COLUMN IF NOT EXISTS state text,
+  ADD COLUMN IF NOT EXISTS city text;
+
+ALTER TABLE user_auth_challenges
+  DROP CONSTRAINT IF EXISTS user_auth_challenges_purpose_check;
+
+ALTER TABLE user_auth_challenges
+  ADD CONSTRAINT user_auth_challenges_purpose_check
+  CHECK (purpose IN ('signup', 'signup_verified', 'login'));
+
+CREATE INDEX IF NOT EXISTS profiles_country_code_idx
+  ON profiles (country_code)
+  WHERE country_code IS NOT NULL;
+
+COMMIT;
