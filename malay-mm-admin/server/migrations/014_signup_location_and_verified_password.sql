dbb20290075747825a8732bb6fbc8431080cddcd
Kyaw san lin <kya295810@gmail.com>
2026-09-26 09:52:29 +0800

    Validate supported signup countries

--- malay-mm-admin/server/migrations/014_signup_location_and_verified_password.sql
+++ malay-mm-admin/server/migrations/014_signup_location_and_verified_password.sql
@@ -13,6 +13,10 @@ ALTER TABLE user_auth_challenges
   ADD CONSTRAINT user_auth_challenges_purpose_check
   CHECK (purpose IN ('signup', 'signup_verified', 'login'));
 
+ALTER TABLE profiles
+  ADD CONSTRAINT profiles_country_code_allowed
+  CHECK (country_code IS NULL OR country_code IN ('MY', 'SG', 'TH'));
+
 CREATE INDEX IF NOT EXISTS profiles_country_code_idx
   ON profiles (country_code)
   WHERE country_code IS NOT NULL;
